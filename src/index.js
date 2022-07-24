f16621329a5c03506307c75cca8c2ac57777a2bf
evgeniyashin <jane.shinich@gmail.com>
2022-07-24 17:03:55 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+const number = Math.abs(n).toString();
+let newNumber = '';
+for (let i=0; i < number.length; i++) {
+newNumber = `${number[i]}${newNumber}`
+}
+return Number(newNumber);
   
 }
